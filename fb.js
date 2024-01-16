4bc1252286585441bdfa1c1e869bf61b98bde25f
saketkrs2021 <96696673+saketkrs2021@users.noreply.github.com>
2024-01-17 01:21:28 +0530

    Add files via upload

---
+++ fb.js
@@ -0,0 +1,9 @@
+
+var finished_rendering = function() {
+  console.log("finished rendering plugins");
+  var spinner = document.getElementById("spinner");
+  spinner.removeAttribute("style");
+  spinner.removeChild(spinner.childNodes[0]);
+}
+FB.Event.subscribe('xfbml.render', finished_rendering);
+
